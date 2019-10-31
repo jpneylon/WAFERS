1c292023f11d4a9602919f6966f88621d1a75d4e
Jacko <jpneylon@users.noreply.github.com>
2019-10-30 18:50:45 -0700

    spc

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var app = new Vue({
   el: '#app',
   components: { vuejsDatepicker },
@@ -7,8 +8,29 @@ var app = new Vue({
     mrn: '',
     rxdose: '',
     fractions: '',
-    txdate: new Date(),
-    rxdate: new Date(),
+    txdate: new Date().toISOString().substr(0, 10),
+    rxdate: new Date().toISOString().substr(0, 10),
+    physician: '',
+    physicist: '',
+    docs:[
+        {'id':'1','name':'Amini, Arya'},
+        {'id':'2','name':'Chen, YiJen'},
+        {'id':'3','name':'Dandapani, Savita'},
+        {'id':'4','name':'Glaser, Scott'},
+        {'id':'5','name':'Radany, Eric'},
+        {'id':'6','name':'Sampath, Sagus'},
+        {'id':'7','name':'Vora, Nayana'},
+        {'id':'8','name':'Wong, Jeffrey'}
+      ],
+    nerds: [
+        {'id':'1','name':'Du, Dongsu'},
+        {'id':'2','name':'Han, Chunhui'},
+        {'id':'3','name':'Liang, Iris'},
+        {'id':'4','name':'Liu, An'},
+        {'id':'5','name':'Neylon, Jack'},
+        {'id':'6','name':'Sun, Sean'},
+        {'id':'7','name':'Zhang, Sean'}
+      ],
   }
 })
 
