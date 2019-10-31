bf3f6ba31957776638238dff3f0f29f643c68d31
Jacko <jpneylon@users.noreply.github.com>
2019-10-31 09:50:26 -0700

    working, no datepicker

--- js/main.js
+++ js/main.js
@@ -1,21 +1,17 @@
-
 var app = new Vue({
   el: '#app',
-  components: { vuejsDatepicker },
+
   data: {
     message: 'Container for Wafers',
     patient: '',
     mrn: '',
     rxdose: '',
     fractions: '',
-    txdate: new Date().toISOString().substr(0, 10),
-    rxdate: new Date().toISOString().substr(0, 10),
+    txdate: null,
+    rxdate: null,
     physician: '',
     physicist: '',
     docs:[
-        {'id':'1','name':'Amini, Arya'},
-        {'id':'2','name':'Chen, YiJen'},
-        {'id':'3','name':'Dandapani, Savita'},
         {'id':'4','name':'Glaser, Scott'},
         {'id':'5','name':'Radany, Eric'},
         {'id':'6','name':'Sampath, Sagus'},
@@ -23,13 +19,13 @@ var app = new Vue({
         {'id':'8','name':'Wong, Jeffrey'}
       ],
     nerds: [
-        {'id':'1','name':'Du, Dongsu'},
-        {'id':'2','name':'Han, Chunhui'},
-        {'id':'3','name':'Liang, Iris'},
-        {'id':'4','name':'Liu, An'},
-        {'id':'5','name':'Neylon, Jack'},
-        {'id':'6','name':'Sun, Sean'},
-        {'id':'7','name':'Zhang, Sean'}
+        {'id':'1','lastname':'Du','firstname':'Dongsu'},
+        {'id':'2','lastname':'Han','firstname':'Chunhui'},
+        {'id':'3','lastname':'Liang','firstname':'Iris'},
+        {'id':'4','lastname':'Liu','firstname':'An'},
+        {'id':'5','lastname':'Neylon','firstname':'Jack'},
+        {'id':'6','lastname':'Sun','firstname':'Sean'},
+        {'id':'7','lastname':'Zhang','firstname':'Sean'}
       ],
   }
 })
