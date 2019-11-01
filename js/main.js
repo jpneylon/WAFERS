257d584ef066a24b6c847bffe01a6ae8f70c3f56
Jacko <jpneylon@users.noreply.github.com>
2019-11-01 09:22:08 -0700

    conditional rendering

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
+Vue.component('wafer-spc', {
+  props: {
+    w_patient: String,
+    w_mrn: String,
+    w_physician: Object,
+    w_physicist: Object,
+    w_rxdate: Date,
+    w_txdate: Date,
+  },
+  template: `
+  <div>
+    <img src='data/cohlogo.jpg'/>
+    <h2>Special Medical Physics Consult Report</h2>
+    <h3>Radiation Oncology</h3>
+    <h3>City of Hope National Medical Center</h3>
+
+    <p>&nbsp;</p>
+    <p>Date: <strong>{{ w_rxdate.toDateString() }}</strong></p>
+    <p>To: Radiation Physicist</p>
+    <p>Request From: <strong>{{ w_physician.firstname }} {{ w_physician.lastname }}, MD</strong></p>
+    <p>Patient Name: <strong>{{ w_patient }}</strong></p>
+    <p>MRN: <strong>{{ w_mrn }}</strong></p>
+    <p>Purpose:</p>
+
+    <p>&nbsp;</p>
+
+    <p>Date: <strong>{{ w_txdate.toDateString() }}</strong></p>
+    <p>To: <strong>{{ w_physician.firstname }} {{ w_physician.lastname }}, MD</strong></p>
+    <p>Purpose:</p>
+
+    <p>&nbsp;</p>
+
+    <h4>{{ w_physicist.firstname }} {{ w_physicist.lastname }}, PhD, DABR</h4>
+    <h4>Radiation Physicist</h4>
+  </div>
+  `
+})
+
 
 var app = new Vue({
   el: '#app',
@@ -12,6 +50,7 @@ var app = new Vue({
     rxdate: new Date(),
     physician: '',
     physicist: '',
+    showSPC: false,
     docs:[
         {'id':'1','lastname':'Amini','firstname':'Arya'},
         {'id':'2','lastname':'Chen','firstname':'YiJen'},
@@ -31,10 +70,11 @@ var app = new Vue({
         {'id':'6','lastname':'Sun','firstname':'Sean'},
         {'id':'7','lastname':'Zhang','firstname':'Sean'}
       ],
+  },
+  methods: {
+    toggleShow: function(showSwitch) {
+      if (showSwitch) return(false)
+      else return(true)
+    }
   }
-})
-
-Vue.component('wafer', {
-  template: `
-  `
 })
