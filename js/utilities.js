f47da06f296f38f169b8816ea13ec6c59e2461af
Jacko <jpneylon@users.noreply.github.com>
2019-11-19 10:24:58 -0800

    working diode check

--- js/utilities.js
+++ js/utilities.js
@@ -5,9 +5,8 @@ Vue.component('report-header', {
   template: `
   <div>
     <img src='data/cohlogo.jpg'/>
+    <h4>Department of Radiation Oncology</h4>
     <h3>{{rh_title}}</h3>
-    <h4>Radiation Oncology</h4>
-    <h4>City of Hope National Medical Center</h4>
   </div>
   `
 })
@@ -18,6 +17,18 @@ Vue.component('two-column-row', {
   <p class='row'>
     <span class='column'>{{tcr_label}}</span>
     <span class='column'><strong>{{tcr_value}}</strong></span>
+  </p>
+  `
+})
+
+Vue.component('four-column-row', {
+  props: ['tcr_label','tcr_value','tcr_label_2','tcr_value_2'],
+  template: `
+  <p class='row'>
+    <span class='column_x4'>{{tcr_label}}</span>
+    <span class='column_x4'><strong>{{tcr_value}}</strong></span>
+    <span class='column_x4'>{{tcr_label_2}}</span>
+    <span class='column_x4'><strong>{{tcr_value_2}}</strong></span>
   </p>
   `
 })
